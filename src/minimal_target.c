8be198b999d6b17ea2256727d54c4b1b0f98c07e
hsaliak <hsaliak@gmail.com>
2026-02-03 22:54:14 -0500

    fix: resolve clang-tidy issues and linker warnings in test targets
    - Replaced 'asm' with '__asm__' for better standards compliance.
    - Renamed 'start' to '_start' to fix linker warnings for -nostdlib.
    - Suppressed 'bugprone-reserved-identifier' for '_start' in test targets.

--- src/minimal_target.c
+++ src/minimal_target.c
@@ -1,8 +1,8 @@
 #include <sys/syscall.h>
 
-void _start() {
+void _start(void) { // NOLINT(bugprone-reserved-identifier)
     // exit(0)
-    asm volatile (
+    __asm__ __volatile__ (
         "movq $60, %%rax\n"
         "movq $0, %%rdi\n"
         "syscall"
--- src/violation_target.c
+++ src/violation_target.c
@@ -1,9 +1,9 @@
 #include <sys/syscall.h>
 
-void _start() {
+void _start(void) { // NOLINT(bugprone-reserved-identifier)
     // getppid (Forbidden)
-    long ret;
-    asm volatile (
+    long ret = 0;
+    __asm__ __volatile__ (
         "movq $110, %%rax\n"
         "syscall"
         : "=a" (ret)
@@ -12,7 +12,7 @@ void _start() {
     );
 
     // exit(0)
-    asm volatile (
+    __asm__ __volatile__ (
         "movq $60, %%rax\n"
         "movq $0, %%rdi\n"
         "syscall"
