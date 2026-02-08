7768be90c0c6d036785ffb14cd20ce9c51f47e50
hsaliak <hsaliak@gmail.com>
2026-02-07 23:19:55 -0500

    Add write syscall to minimal_target assembly
    Rationale: The `minimal_target` used for integration tests was a bare-metal assembly program that only called `exit`. The generator test expects a `write` syscall to be present in the generated policy.
    Updated `minimal_target` to include a `write` syscall before exiting.

--- src/minimal_target.c
+++ src/minimal_target.c
@@ -1,13 +1,20 @@
 #include <sys/syscall.h>
 
 void _start(void) { // NOLINT(bugprone-reserved-identifier)
-    // exit(0)
+    // write(1, "hello\n", 6)
     __asm__ __volatile__ (
+        "movq $1, %%rax\n"
+        "movq $1, %%rdi\n"
+        "lea msg(%%rip), %%rsi\n"
+        "movq $6, %%rdx\n"
+        "syscall\n"
+        // exit(0)
         "movq $60, %%rax\n"
         "movq $0, %%rdi\n"
-        "syscall"
+        "syscall\n"
+        "msg: .ascii \"hello\\n\""
         :
         :
-        : "rax", "rdi", "rcx", "r11", "memory"
+        : "rax", "rdi", "rsi", "rdx", "rcx", "r11", "memory"
     );
 }
